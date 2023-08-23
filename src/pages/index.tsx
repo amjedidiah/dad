44edbfcf2f35a494d3411233603854c880fd2ccf
Jedidiah Amaraegbu <imunacode@gmail.com>
2023-08-23 13:30:01 +0100

    feat: payment verification for paystack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import Jumbo from "@/components/landing/jumbo/index.jumbo";
 import Layout from "@/components/shared/layout";
+import useVerifyPaystackPayment from "@/hooks/use-verify-paystack-payment";
 
 export default function Home() {
+  useVerifyPaystackPayment();
+
   return (
     <Layout
       title="Dr Passy Amaraegbu | Living a life of purity, power and prosperity"
